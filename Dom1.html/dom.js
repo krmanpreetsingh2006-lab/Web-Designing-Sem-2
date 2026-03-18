7de1774f086ddde68f351448771bb47bfaed0500
krmanpreetsingh2006-lab <krmanpreetsingh2006@gmail.com>
2026-03-18 17:28:41 +0530

    CM

---
+++ Dom1.html/dom.js
@@ -0,0 +1,7 @@
+let newHead = document.getElementById("container");
+newHead.textContent = "Welcome to the Student Marks Manager!";
+newHead.style.color = "#527dae";
+let newdiv = document.createElement("div");
+newdiv.textContent = "Introduction to DOM Manipulation";
+document.body.appendChild(newdiv);
+newdiv.textContent = "This is a simple example of how to manipulate the DOM using JavaScript.";
