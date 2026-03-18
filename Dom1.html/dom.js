cb16c10064f6f42256e4dcc29d00ca0443b5a2b8
krmanpreetsingh2006-lab <krmanpreetsingh2006@gmail.com>
2026-03-18 17:30:43 +0530

    CM

--- Dom1.html/dom.js
+++ Dom1.html/dom.js
@@ -5,3 +5,5 @@ let newdiv = document.createElement("div");
 newdiv.textContent = "Introduction to DOM Manipulation";
 document.body.appendChild(newdiv);
 newdiv.textContent = "This is a simple example of how to manipulate the DOM using JavaScript.";
+newdiv.style.fontSize = "18px";
+newdiv.style.color = "#333";
