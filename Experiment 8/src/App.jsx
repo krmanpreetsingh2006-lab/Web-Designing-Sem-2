8089a1612745a813950584a248d6dd98d6407b26
krmanpreetsingh2006-lab <krmanpreetsingh2006@gmail.com>
2026-06-18 18:00:31 +0530

    Exp8 commit

---
+++ Experiment 8/src/App.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+   
+  const reset = () => {
+    setCount(0);
+  };
+  
+  return (
+    <div className="container">
+      <h1>React Counter</h1>
+      <h2>{count}</h2>
+    <div className="Counter">
+    <button onClick={increment}>Increment</button>
+    <button onClick={decrement}>Decrement</button>
+    <button onClick={reset}>Reset</button>    
+    
+    </div>
+    </div>
+  );
+}
+  export default App;
